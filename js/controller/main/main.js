f25fbde7fce7708218022578e60f6db257fbbb5c
kaisa <7083620@hanmail.net>
2019-07-30 14:43:31 +0900

    ..

--- js/controller/main/main.js
+++ js/controller/main/main.js
@@ -13,9 +13,10 @@
 			{url : '/img/main/visual3.jpg'},
 			/*{url : '/img/main/visual1.jpg'},*/
 			{url : '/img/main/visual4.jpg'},
+			{url : '/img/main/visual21.png'},
+			{url : '/img/main/visual22.png'}
 			//{url : '/img/main/visual5.jpg'},
 			//{url : '/img/main/visual2.jpg'}
-
 		];
 		$scope.visual = {
 			idx : 0,
